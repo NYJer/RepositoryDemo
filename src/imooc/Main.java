78a1e0c984620e05ffb896437a3942d005acd1b5
NYJer <18602480585@163.com>
2020-03-02 19:33:09 +0800

    在master分支下增加一个sayHello方法

--- src/imooc/Main.java
+++ src/imooc/Main.java
@@ -1,8 +1,13 @@
 package imooc;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("HelloWorld");
+    }
+    public void sayHello()
+    {
+        System.out.println("sayHello master");
     }
 
 
